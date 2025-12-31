42e0da77ef2b2ceacb13d6a52d0eac66a3af7d56
Ashvitharavichandran <ashvitharavichand@gmail.com>
2025-12-31 13:51:54 +0530

    Add files via upload

---
+++ Customer.sql
@@ -0,0 +1,15 @@
+-- SQL statement to join dim_customers with dim_geography to enrich customer data with geographic information
+
+SELECT 
+    c.CustomerID,  
+    c.CustomerName, 
+    c.Email, 
+    c.Gender,  
+    c.Age,  
+    g.Country, 
+    g.City 
+FROM 
+    customers AS c  
+LEFT JOIN 
+    geography AS g 
+    ON c.GeographyID = g.GeographyID;
